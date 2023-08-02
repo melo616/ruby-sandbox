af3a6a202a04fb551e32fa9c2c77be99f00a6d6d
Katherine Jarboe <110368535+melo616@users.noreply.github.com>
2023-08-02 17:41:22 +0000

    added debugger

--- howdy.rb
+++ howdy.rb
@@ -1,4 +1,10 @@
-my_string = "Howdy, world!"
-pp my_string
+require "active_support/all"
+
+# my_string = "Howdy, world!"
+# pp my_string
+
+pp "What's your name?"
+their_name = gets.chomp
+puts "Howdy, " + their_name + "!"
 
 require "./goodbye.rb"
